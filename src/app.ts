459cf88e6c97a0f73edd6eb65c780f581a3e1f30
TheAndrey777 <andreyvasilchenko777@mail.ru>
2024-11-23 00:01:00 +0300

    feat(backend): cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware";
 import { authRouter } from "./routes/auth.router";
 import "./strategies/jwt.strategy";
 
 const app: Application = express();
+
+app.use(cors({ credentials: true }));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
