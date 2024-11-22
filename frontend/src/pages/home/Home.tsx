5480f4bb9efd18197a544fbf52465d746858668c
Виталий <114582703+valavshonok@users.noreply.github.com>
2024-11-22 21:38:22 +0300

    feat(frontend): main container

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,9 +1,23 @@
-import Menu from "../../components/navigation/menu/Menu";
+import React from "react";
+import { cn } from "../../lib/cn";
 
 const Home = () => {
+  const [a, seta] = React.useState("");
   return (
-    <div>
-      <Menu />
+    <div className="bg-default-100 bg-red-200 min-h-full w-full relative grid grid-cols-[auto_1fr] gap-[20px] p-[25px] box-border ">
+      <div
+        className={cn(
+          "text-content-1 bg-layout-background w-[20rem] h-full transition-all ",
+          a
+        )}
+        onClick={() => {
+          seta("w-[40px]");
+          if (a == "w-[40px]") seta("");
+        }}
+      >
+        Типо меню
+      </div>
+      <div className="bg-green-300 w-full h-[2000px]"> asdf</div>
     </div>
   );
 };
