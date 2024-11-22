67728f85a142a825eb816a06214d25bca55af8ce
TheAndrey777 <andreyvasilchenko777@mail.ru>
2024-11-22 19:16:07 +0300

    feat(backend): db connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import "dotenv/config";
 import app from "./app";
+import { dataSource } from "./data-source";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+async function init() {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+  
+  await dataSource.initialize()
+    .catch((err) => {
+      console.error(err);
+      return;
+    });
+  console.log("Successfully connected to the database");
+}
+
+init();
