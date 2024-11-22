3dd15bacaf669b6cae02b6b65cb4db92fb67ec48
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-11-22 20:03:17 +0300

    chart(frontend): rename main pixi component

--- frontend/src/components/pixi/PixiApplication.tsx
+++ frontend/src/components/pixi/PixiApplication.tsx
@@ -1,7 +1,7 @@
 import { Stage } from "@pixi/react";
 import { Grid } from "./Grid";
 
-export const App = () => (
+export const PixiApplication = () => (
   <Stage width={500} height={500}>
     <Grid size={20} height={500} width={500}></Grid>
   </Stage>
