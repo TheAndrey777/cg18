e74bfabba8129591700c4b36a0f738b1944c3210
JForea <138843512+JForea@users.noreply.github.com>
2024-11-22 20:40:37 +0300

    feat(frontend): menu

--- frontend/src/components/navigation/menu/Menu.tsx
+++ frontend/src/components/navigation/menu/Menu.tsx
@@ -3,9 +3,14 @@ import { MenuItem } from "./MenuItem";
 import ProfileSVG from "../../../assets/svg/profile.svg";
 
 export const Menu: React.FC = () => {
+  let pageNames: string[] = ["Профиль", "Офисы"];
+  let currentPage = 1;
+
   return (
-    <div className="ProfileSVG">
-      <MenuItem name="test" icon={ProfileSVG}></MenuItem>
+    <div className="w-[320px] h-screen bg-white m-[20px] rounded-[15px]">
+      {pageNames.map((name: string, i: number) => {
+        return <MenuItem name={name} isActive={i === currentPage}></MenuItem>;
+      })}
     </div>
   );
 };
--- frontend/src/components/navigation/menu/MenuItem.tsx
+++ frontend/src/components/navigation/menu/MenuItem.tsx
@@ -2,14 +2,30 @@ import React from "react";
 
 interface MenuItemProps {
   name: string;
-  icon: any;
+  icon?: any;
+  isActive: boolean;
+  onClick?: () => void;
 }
 
-export const MenuItem: React.FC<MenuItemProps> = ({ name, icon }) => {
+export const MenuItem: React.FC<MenuItemProps> = ({
+  name,
+  icon,
+  isActive,
+  onClick,
+}) => {
   return (
-    <div className="text-black flex">
-      <img className="w-20" src={icon} />
-      <div>{name}</div>
+    <div className="pl-[10px]" onClick={onClick}>
+      <div
+        className={
+          "w-[160px] h-[40px] text-content-1 flex m-[10px] p-[5px] items-center rounded-[10px] select-none " +
+          (isActive
+            ? "bg-default-200"
+            : "hover:bg-default-100 hover:cursor-pointer")
+        }
+      >
+        {icon != null ? <img className="w-8 mr-[10px] p" src={icon} /> : <></>}
+        <div className="text-[20px] font-medium">{name}</div>
+      </div>
     </div>
   );
 };
