fc655bfe1e353a95c3a7222103bad62248502c9f
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-11-23 00:51:48 +0300

    feat(frontend): Add Pixi mouse

---
+++ frontend/src/PixiMouse/Mouse.ts
@@ -0,0 +1,18 @@
+import { useApp } from "@pixi/react";
+import { Application } from "pixi.js";
+
+const SCREEN_WIDTH = 3162;
+const SCREEN_HEIGHT = 1779;
+
+export const Mouse = {
+  x: (app: number) => {
+    const pointer = app.renderer.plugins.interaction.pointer;
+    let scale = app.stage.scale;
+    return pointer.screenX * scale.x;
+  },
+  y: (app: any) => {
+    const pointer = app.renderer.plugins.interaction.pointer;
+    let scale = app.stage.scale;
+    return pointer.screenY * scale.y;
+  },
+};
