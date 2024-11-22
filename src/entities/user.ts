296d35a2db9ae6e4c7bb9bf9fecca8aa02dc1bef
TheAndrey777 <andreyvasilchenko777@mail.ru>
2024-11-22 21:41:54 +0300

    feat(backend): global error handler

---
+++ src/entities/user.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("users")
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "varchar",
+    length: 32,
+    nullable: false,
+    unique: true
+  })
+  username!: string;
+
+  @Column({
+    type: "varchar",
+    length: 128,
+    nullable: false
+  })
+  password!: string;
+
+  @Column({
+    type: "bool",
+    nullable: false,
+    default: false
+  })
+  admin!: boolean;
+}
