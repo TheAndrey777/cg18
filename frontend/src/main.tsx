d4ad8304fc6defacc655c924e06db43343e7b132
Виталий <114582703+valavshonok@users.noreply.github.com>
2024-11-22 20:34:39 +0300

    fix(frontend): redux store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./styles/index.css"
-import "./styles/palette/theme-dark.css"
-import "./styles/palette/theme-light.css"
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import "./styles/palette/theme-dark.css";
+import "./styles/palette/theme-light.css";
+import store from "./redux/store.ts";
+import { Provider } from "react-redux";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
