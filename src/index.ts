b46eec87f78e334409c6258bcbb33b98dd1a6384
TheAndrey777 <andreyvasilchenko777@mail.ru>
2024-11-22 17:53:28 +0300

    feat(backend): express base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("1");
+import "dotenv/config";
+import app from "./app";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
