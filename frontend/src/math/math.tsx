7f02b837b241177f258648d41e4a7799116b1d34
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-11-23 09:53:23 +0300

    chore(frontend):Add smart intersection

--- frontend/src/math/math.tsx
+++ frontend/src/math/math.tsx
@@ -35,12 +35,12 @@ export const min = (x: number, y: number) => {
 export const isInter = (v1: Vector, v2: Vector) => {
   if (
     max(min(v1.sx, v1.fx), min(v2.sx, v2.fx)) >
-    min(max(v1.sx, v1.fx), max(v2.sx, v2.fx)) - 6
+    min(max(v1.sx, v1.fx), max(v2.sx, v2.fx))
   )
     return false;
   if (
     max(min(v1.sy, v1.fy), min(v2.sy, v2.fy)) >
-    min(max(v1.sy, v1.fy), max(v2.sy, v2.fy)) - 6
+    min(max(v1.sy, v1.fy), max(v2.sy, v2.fy))
   )
     return false;
 
@@ -48,6 +48,32 @@ export const isInter = (v1: Vector, v2: Vector) => {
   const v1_2 = new Vector(v1.sx, v1.sy, v2.fx, v2.fy);
   const v2_1 = new Vector(v2.sx, v2.sy, v1.sx, v1.sy);
   const v2_2 = new Vector(v2.sx, v2.sy, v1.fx, v1.fy);
+
+  const mnX = max(min(v1.sx, v1.fx), min(v2.sx, v2.fx));
+  const mxX = min(max(v1.sx, v1.fx), max(v2.sx, v2.fx));
+  const mnY = max(min(v1.sy, v1.fy), min(v2.sy, v2.fy));
+  const mxY = min(max(v1.sy, v1.fy), max(v2.sy, v2.fy));
+
+  if (
+    v2.sx === v2.fx &&
+    min(Math.abs(v2.sx - v1.sx), Math.abs(v2.sx - v1.fx)) <= 10
+  ) {
+    console.log(
+      mnX,
+      v1.sx,
+      v1.fx,
+      v2.sx,
+      v2.fx,
+      Math.abs(mnX - v1.sx),
+      Math.abs(mxX - v1.fx)
+    );
+    return false;
+  }
+  if (
+    v2.sy === v2.fy &&
+    min(Math.abs(v2.fy - v1.sy), Math.abs(v2.fy - v1.fy)) <= 10
+  )
+    return false;
 
   return !(
     getSign(getVectorMult(v1, v1_1)) * getSign(getVectorMult(v1, v1_2)) > 0 ||
