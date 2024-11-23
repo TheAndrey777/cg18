f42a2f3c4aebaa3cd85f7653bc8ddc50aec7d886
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-11-24 01:16:51 +0300

    chore(frontend): fix merge

--- frontend/src/components/pixi/App.tsx
+++ frontend/src/components/pixi/App.tsx
@@ -11,6 +11,10 @@ export const toolChange = (id: number) => {};
 
 interface Props {}
 
+export const toolChange = (id: number) => {
+  console.log(id);
+};
+
 function PixiApplication(props: Props) {
   const {} = props;
 
