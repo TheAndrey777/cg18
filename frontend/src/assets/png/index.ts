0e5f31971b5f2dd2cd054063bc0915f6d1a1c79d
Виталий <114582703+valavshonok@users.noreply.github.com>
2024-11-23 03:57:25 +0300

    feat(frontend): add img

---
+++ frontend/src/assets/png/index.ts
@@ -0,0 +1,7 @@
+import officeicon from "./officeicon.png";
+import worker1 from "./worker1.png";
+import worker2 from "./worker2.png";
+import worker3 from "./worker3.png";
+import worker4 from "./worker4.png";
+
+export { officeicon, worker1, worker2, worker3, worker4 };
