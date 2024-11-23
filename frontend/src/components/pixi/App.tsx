eba26f3f3ec7f4204ae2b1655bde0b044a8181e3
RomaTheKing <103886943+RomaTheKing@users.noreply.github.com>
2024-11-23 06:20:13 +0300

    chore(frontend): add line and math

--- frontend/src/components/pixi/App.tsx
+++ frontend/src/components/pixi/App.tsx
@@ -1,19 +1,54 @@
 import { Stage, useApp } from "@pixi/react";
 import { Grid } from "./Grid";
+import React, { useRef, useEffect } from "react";
+import { Application, Container, Graphics } from "pixi.js";
 
 interface Props {}
 
 function PixiApplication(props: Props) {
   const {} = props;
 
+  const ref = useRef(null);
+
+  useEffect(() => {
+    // On first render create our application
+    const app = new Application({
+      width: 500,
+      height: 500,
+      backgroundColor: 0x5bba6f,
+    });
+
+    // Add app to DOM
+    ref.current!.appendChild(app.view);
+
+    app.stage.addChild(new Grid(500, 500, 30, app));
+
+    app.ticker.add(() => {}, 100);
+
+    // Start the PixiJS app
+    app.start();
+
+    return () => {
+      // On unload completely destroy the application and all of it's children
+      app.destroy(true, true);
+    };
+  }, []);
+
   return (
     <div onContextMenu={(e) => e.preventDefault()}>
       <span>123321</span>
-      <Stage width={500} height={500} renderOnComponentChange={true}>
-        <Grid size={20} height={500} width={500}></Grid>
-      </Stage>
+      <div ref={ref} />
     </div>
   );
+
+  // return (
+  //   <div onContextMenu={(e) => e.preventDefault()}>
+  //     <span>123321</span>
+  //     <Stage width={500} height={500} renderOnComponentChange={true}>
+  //       <Grid size={20} height={500} width={500}></Grid>
+  //     </Stage>
+  //   </div>
+  // );
 }
 
 export default PixiApplication;
---
+++ frontend/src/math/math.tsx
@@ -0,0 +1,56 @@
+export class Vector {
+  public sx: number;
+  public sy: number;
+  public fx: number;
+  public fy: number;
+  public x: number;
+  public y: number;
+
+  constructor(sx: number, sy: number, fx: number, fy: number) {
+    this.sx = sx;
+    this.sy = sy;
+
+    this.fx = fx;
+    this.fy = fy;
+
+    this.x = fx - sx;
+    this.y = fy - sy;
+  }
+}
+
+export const getVectorMult = (v1: Vector, v2: Vector) => {
+  return v1.x * v2.y - v2.x * v1.y;
+};
+
+export const getSign = (x: number) => (x ? x / Math.abs(x) : x);
+
+export const max = (x: number, y: number) => {
+  return x > y ? x : y;
+};
+
+export const min = (x: number, y: number) => {
+  return x < y ? x : y;
+};
+
+export const isInter = (v1: Vector, v2: Vector) => {
+  if (
+    max(min(v1.sx, v1.fx), min(v2.sx, v2.fx)) >
+    min(max(v1.sx, v1.fx), max(v2.sx, v2.fx))
+  )
+    return false;
+  if (
+    max(min(v1.sy, v1.fy), min(v2.sy, v2.fy)) >
+    min(max(v1.sy, v1.fy), max(v2.sy, v2.fy))
+  )
+    return false;
+
+  const v1_1 = new Vector(v1.sx, v1.sy, v2.sx, v2.sy);
+  const v1_2 = new Vector(v1.sx, v1.sy, v2.fx, v2.fy);
+  const v2_1 = new Vector(v2.sx, v2.sy, v1.sx, v1.sy);
+  const v2_2 = new Vector(v2.sx, v2.sy, v1.fx, v1.fy);
+
+  return !(
+    getSign(getVectorMult(v1, v1_1)) * getSign(getVectorMult(v1, v1_2)) >= 0 ||
+    getSign(getVectorMult(v2, v2_1)) * getSign(getVectorMult(v2, v2_2)) >= 0
+  );
+};
