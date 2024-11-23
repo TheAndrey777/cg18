27ac8d05be7ed6a3596a748af5d7d948b145958c
Виталий <114582703+valavshonok@users.noreply.github.com>
2024-11-24 00:39:44 +0300

    fix(frontend): add users modal

--- frontend/src/views/offices/Management.tsx
+++ frontend/src/views/offices/Management.tsx
@@ -10,6 +10,7 @@ import { Input } from "../../components/input/Input";
 import { close } from "../../assets/svg";
 import Spiner from "../../components/spiner/Spiner";
 import { worker1, worker2, worker3, worker4 } from "../../assets/png";
+import { getAllUsers } from "../../redux/slices/user";
 
 const Management = () => {
   const createStatus = useSelector((state: any) => state.office.add.status);
@@ -21,14 +22,27 @@ const Management = () => {
 
   const { officeId } = useParams();
   const [active, setActive] = React.useState(0);
+  const [activeValid, setActiveValid] = React.useState(0);
+
+  const [username, setUsername] = React.useState("");
 
   const workers = useSelector((state: any) => state.office.offices).filter(
     (x: any) => x.id == officeId
   );
 
+  const users = useSelector((state: any) => state.user.users);
+  const [validUsers, setValidUsers] = React.useState([]);
+  console.log(users);
+  console.log("val", validUsers);
+
   React.useEffect(() => {
     dispatch(getOffice());
+    dispatch(getAllUsers());
   }, []);
+
+  React.useEffect(() => {
+    setValidUsers(users.filter((s: any) => s.email.includes(username)));
+  }, [username]);
 
   return (
     <>
@@ -43,7 +57,9 @@ const Management = () => {
                 Данная вкладка позволяет вам легко управлять персоналом вашего
                 офиса
               </div>
+              {}
             </div>
+
             <Button
               color="default"
               onClick={() => {
@@ -83,6 +99,7 @@ const Management = () => {
                 workers[0].workers &&
                 workers[0].workers.map((v: any, i: number) => (
                   <div
+                    key={i}
                     className={cn(
                       "grid grid-cols-[1fr_4fr_4fr] h-[40px] cursor-pointer hover:bg-default-100 hover:scale-[1.02] active:scale-[.98] transition-all text-content-1 rounded-[10px]",
                       active == i && "bg-default-100"
@@ -123,7 +140,7 @@ const Management = () => {
         backdrop="opaque"
         className=" overflow-hidden"
       >
-        <div className="h-[350px] w-[500px] bg-layout-background rounded-[15px] relative">
+        <div className=" w-[500px] h-[490px] bg-layout-background rounded-[15px] relative">
           {createStatus == "loading" && (
             <div className=" absolute h-full w-full flex items-center justify-center bg-layout-divider z-50">
               <Spiner />
@@ -149,6 +166,43 @@ const Management = () => {
                 Найдите пользователя, которого хотите добавить в ваш офис, и
                 нажмите на кнопку Пригласить
               </div>
+
+              <div className="w-full h-[270px]  ">
+                <Input
+                  onChange={setUsername}
+                  size="md"
+                  radius="sm"
+                  label="Почта пользователя"
+                  className="mb-[10px]"
+                />
+                {validUsers &&
+                  validUsers.slice(0, 5).map((v: any, i: number) => (
+                    <div
+                      key={i}
+                      className={cn(
+                        "grid grid-cols-[1fr_8fr_8fr] h-[40px] cursor-pointer hover:bg-default-100 hover:scale-[1.02] active:scale-[.98] transition-all text-content-1 rounded-[10px]",
+                        activeValid == i && "bg-default-100"
+                      )}
+                      onClick={() => setActiveValid(i)}
+                    >
+                      <div className="h-[40px] w-full flex items-center justify-center ">
+                        {i + 1}
+                      </div>
+                      <div className="h-[40px] w-full flex items-center pl-[10px] box-border">
+                        <img
+                          className=" select-none h-[30px] w-[30px] rounded-full border-solid border-[2px] border-primary box-border m-[5px] mr-[10px]"
+                          src={icons[i % 4]}
+                          alt="logo"
+                        />
+                        {v.name}
+                      </div>
+                      <div className="h-[40px] w-full flex items-center  pl-[10px] box-border">
+                        {v.email}
+                      </div>
+                    </div>
+                  ))}
+              </div>
+
               <div className="w-[calc(100%-80px)] flex absolute  h-[75px] items-center justify-end">
                 <Button
                   className="m-4"
