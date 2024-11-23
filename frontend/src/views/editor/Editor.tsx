b49805fec67ffa8a6f17e25d95522a68037f3963
Виталий <114582703+valavshonok@users.noreply.github.com>
2024-11-23 03:58:20 +0300

    feat(frontend): editor view

---
+++ frontend/src/views/editor/Editor.tsx
@@ -0,0 +1,13 @@
+import PixiApplication from "../../components/pixi/App";
+
+const Editor = () => {
+  return (
+    <div className="w-full h-full">
+      <div className="bg-blue-200">
+        <PixiApplication />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
